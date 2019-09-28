832eaa871c65bdfcd98bc342833748cd361fad90
haydenmlh <haydenmlh@gmail.com>
2019-09-28 19:43:36 -0400

    A primitive discord bot which responds to some commands.
    A Primitive Discord bot which responds to some commands by the discord user on the server which the bot is on.

---
+++ bot.py
@@ -0,0 +1,60 @@
+# bot.py
+import os
+import random
+import pizzapi
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} has connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+
+    print(f'{guild.members}\n')
+    print(f'testing\n{client.user}\n')
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server'
+    )
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+        print('sending message')
+    elif message.content == 'raise-exception':
+        raise discord.DiscordException
+
+client.run(TOKEN)
---
+++ bot_bot.py
@@ -0,0 +1,43 @@
+# bot_bot.py
+import os
+import random
+from dotenv import load_dotenv
+
+# 1
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+# 2
+bot = commands.Bot(command_prefix='#')
+
+@bot.event
+async def on_ready():
+    for guild in bot.guilds:
+        if guild.name == GUILD:
+            break
+    print(
+        f'{bot.user.name} has connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+
+@bot.command(name='order_pizza', help='Doesn\'t order pizza')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+bot.run(TOKEN)
